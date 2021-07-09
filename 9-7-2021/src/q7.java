0d7c02099c7279b281424c20c2031d881360d4a1
Rohit <Rohit@Rohit>
2021-07-09 14:45:46 +0530

    all questions completed

--- 9-7-2021/src/q7.java
+++ 9-7-2021/src/q7.java
@@ -2,7 +2,21 @@
 public class q7 {
 
 	public static void main(String[] args) {
-			
+			int limit = 500;
+			int var = 2;
+			int a, b, c =0;
+			while(var<limit) {
+				for(int i=1; i<var; i++) {
+					a = var*var - i*i;
+					b = 2*i*var;
+					c = i*i + var*var;
+					if(c>limit) {
+						break;
+					}
+					System.out.println(a + " " + b + " " + c);
+				}
+				var++;
+			}
 	}
 
 }
