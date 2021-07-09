86cfff5f5eabb9859e51da1651b5db33ceadd87a
Rohit <Rohit@Rohit>
2021-07-09 11:31:05 +0530

    q2 updated

--- 9-7-2021/src/q2.java
+++ 9-7-2021/src/q2.java
@@ -4,7 +4,12 @@ public class q2 {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		hjjlkj
+		System.out.println("Enter a number");;
+		int n = scn.nextInt();
+		while(n>0) {
+			System.out.print(n%10 + " ");
+			n = n/10;
+		}
 	}
 
 }
