2c79ee95fa40944787a9a9c40c1fd9ac22826aab
Rohit <Rohit@Rohit>
2021-07-09 11:47:19 +0530

    q2 done

--- 9-7-2021/src/q2.java
+++ 9-7-2021/src/q2.java
@@ -6,10 +6,23 @@ public class q2 {
 		Scanner scn = new Scanner(System.in);
 		System.out.println("Enter a number");;
 		int n = scn.nextInt();
+		int div = 10000;
 		while(n>0) {
-			System.out.print(n%10 + " ");
-			n = n/10;
+			if(n/div<10&&n/div>0) {
+			System.out.print(n/div + " ");
+			}
+			else if(n/div==0) {
+				System.out.println("number has less than five digits");
+				break;
+			}
+			else {
+					System.out.println("number has more than five digits");
+					break;
+			}
+			n = n%div;
+			div = div/10;
 		}
-	}
+		System.out.println();
+		}
 
 }
