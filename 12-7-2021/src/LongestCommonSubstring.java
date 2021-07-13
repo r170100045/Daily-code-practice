2141792f3ef1ab525ba66a87db3e6f5ec49df4cb
Rohit <Rohit@Rohit>
2021-07-13 22:39:40 +0530

    Array and String assignment done

--- 12-7-2021/src/LongestCommonSubstring.java
+++ 12-7-2021/src/LongestCommonSubstring.java
@@ -1,12 +1,34 @@
 import java.util.Scanner;
 
 public class LongestCommonSubstring {
-	static String getLCS(String str) {
+	static String getLCS(String[] arr) {
+		int n = arr.length;
+		String s1 = arr[0];
+		String lcs = "";
+		int len = s1.length();
+		for(int i=0; i<len; i++) {
+			for(int j=i+1; j<=len; j++) {
+				String stem = s1.substring(i, j);
+				int count=0;
+				for(int k=1; k<n; k++) {
+					count++;
+					if(!arr[k].contains(stem))
+						break;
+				}
+				if(count==n-1 && lcs.length()<stem.length()) {
+					lcs = stem;
+					
+				}
+			}
+		}
+		return lcs;
+
 		
 	}
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String str = scn.nextLine();
+		String arr[] = { "grace", "graceful", "disgraceful","gracefully" };
+		String stems = getLCS(arr);
+		System.out.println(stems);
 		
 	}
 
