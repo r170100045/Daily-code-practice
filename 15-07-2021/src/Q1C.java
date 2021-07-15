1dd159a3ed044cd2daee32a4d135a5d15f4d7041
Rohit <Rohit@Rohit>
2021-07-15 22:35:13 +0530

    1c updated

--- 15-07-2021/src/Q1C.java
+++ 15-07-2021/src/Q1C.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
 
 public class Q1C {
-	public static void checkException() throws IOException{
-		throw new IOException("Exception handled");
+	public static void checkException() {
+		throw new IOException("Exception handled"); 
+		/* if we don't throw the exception it will cause compilation problem 
+		 * hence we must use 'throws' keyword in such case, hence it will show error at compilation time
+		 */
 	}
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)  {
 		Q1C var = new Q1C();
-		var.checkException();
+		Q1C.checkException(); 
 		System.out.println("rest of code");
 	}
 
