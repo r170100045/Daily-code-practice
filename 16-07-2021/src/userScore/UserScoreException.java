cbb7c247ca5f86bb0b888d4f454a271c4c009752
Rohit <Rohit@Rohit>
2021-07-16 09:47:24 +0530

    updated

--- 16-07-2021/src/userScore/UserScoreException.java
+++ 16-07-2021/src/userScore/UserScoreException.java
@@ -20,24 +20,48 @@ public class UserScoreException {
 				}catch (ScoreException e) {
 					System.out.println(e);
 					System.out.println("The score must be >= 0 and <= 100!");
-					System.out.println("Do you want to enter another score?");
-					String response = scn.nextLine();
-					if(response.equals("Y")||response.equals("Yes")) {
+					String response = new String();
+					while(true) {
+						System.out.println("Do you want to enter another score?");
+						response = scn.nextLine();
+						if(response.equals("Y")||response.equals("Yes")) {
+							break;
+						}else if(response.equals("No")) {
+							break;
+						}else {
+							System.out.println("enter valid input");
+							continue;
+						}
+					}if(response.equals("Y")||response.equals("Yes")) {
 						continue;
 					}else if(response.equals("No")) {
+						System.out.println("Bye!");
 						return;
 					}
+					
 				}
 			}catch(NumberFormatException e) {
 				System.out.println(e);
 				System.out.println("You must enter a number for the score!");
-				System.out.println("Do you want to enter another score?");
-				String response1 = scn.nextLine();
-				if(response1.equals("Y")||response1.equals("Yes")) {
+				String response = new String();
+				while(true) {
+					System.out.println("Do you want to enter another score?");
+					response = scn.nextLine();
+					if(response.equals("Y")||response.equals("Yes")) {
+						break;
+					}else if(response.equals("No")) {
+			break;
+					}else {
+						System.out.println("enter valid input");
+						continue;
+					}
+				}if(response.equals("Y")||response.equals("Yes")) {
 					continue;
-				}else if(response1.equals("No")) {
+				}else if(response.equals("No")) {
+					System.out.println("Bye!");
 					return;
 				}
+				
 			}
 			System.out.println("User entered a valid score");
 			break;
