68a5a3c5bcffec97b2b8e635491f100b63e0c1bf
Rohit <Rohit@Rohit>
2021-07-17 19:53:01 +0530

    Phone number address done

--- 17-07-2021/src/PhoneNumberAddress.java
+++ 17-07-2021/src/PhoneNumberAddress.java
@@ -6,10 +6,15 @@ public class PhoneNumberAddress {
 		char[][] letters = {{'A', 'B', 'C'}, {'D', 'E', 'F'}, {'G', 'H', 'I'}, {'J', 'K', 'L'}, {'M', 'N', 'O'}, 
 				{'P', 'R', 'S'}, {'T', 'U', 'V'}, {'W', 'X', 'Y'}};
 		Scanner scn =  new Scanner(System.in);
-		int first = scn.nextInt();
+		System.out.println("Enter the first digit");
+		int first = scn.nextInt();// first number of the four digit number
+		System.out.println("Enter the second digit");
 		int second = scn.nextInt();
+		System.out.println("Enter the third digit");
 		int third = scn.nextInt();
+		System.out.println("Enter the fourth digit");
 		int fourth = scn.nextInt();
+		int count = 1;// for numbering the letter combination
 		for(int i=0; i<3; i++) {
 			for(int j=0; j<3; j++) {
 				for(int k=0; k<3; k++) {
@@ -18,12 +23,14 @@ public class PhoneNumberAddress {
 						br.write(letters[second-2][j]);
 						br.write(letters[third-2][k]);
 						br.write(letters[fourth-2][l]);
-						br.write("\n");
+						br.write(" "+count+"\n");
+						count++;
 					}
 				}
 			}
 		}
 		br.close();
+		scn.close();
 	}
 
 
