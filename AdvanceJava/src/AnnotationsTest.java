0da5e53e96bd1849812df3140c4889a7b8e8ea2f
r170100045 <rohit.iitb17@gmailcom>
2021-08-31 22:13:14 +0530

    annotaion assignment done

--- AdvanceJava/src/AnnotationsTest.java
+++ AdvanceJava/src/AnnotationsTest.java
@@ -1,8 +1,48 @@
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+@Retention (RetentionPolicy.RUNTIME)
+@interface isEmployee{}
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Location{
+	String location() default "Hyderabad";
+}
+
+@Retention (RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface CompanyDetails{
+	String nameOfCompany() default "Beehyv";
+	int sizeOfCompany() default 200;
+}
+
+@isEmployee
+@CompanyDetails(nameOfCompany="Google")
+class Employee{
+	int age;
+	int salary;
+	
+	public Employee(int age, int salary) {
+		this.age = age;
+		this.salary = salary;
+	}
+
+	@Location(location="Telangana")
+	public void locationFromAnnotaion(){}
+}
 public class AnnotationsTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
+		Employee emp = new Employee(25,50000);
+		Method m = emp.getClass().getMethod("locationFromAnnotaion");
+		Location loc = m.getAnnotation(Location.class);
+		Class empClass = emp.getClass();
+		if(empClass.isAnnotationPresent(isEmployee.class)) {
+			System.out.println("yes our object is an employee as the isEmployee Annotaion is present");
+		}
+		CompanyDetails cd = (CompanyDetails) empClass.getAnnotation(CompanyDetails.class);
+		System.out.println("Location of Employee is " + loc.location());
+		System.out.println("Name of company is " + cd.nameOfCompany() + " and number of Employees in company are " + cd.sizeOfCompany());
 
 	}
 
